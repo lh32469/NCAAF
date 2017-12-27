dbcb1685c246ecf3710cbc9e74e87de747577765
lyle.harris@oracle.com <lyle.harris@oracle.com>
2017-12-27 13:55:59 -0800

    WIP towards more generic views

---
+++ src/main/java/org/gpc4j/ncaaf/views/CFB_View.java
@@ -0,0 +1,14 @@
+package org.gpc4j.ncaaf.views;
+
+/**
+ *
+ * @author Lyle T Harris
+ */
+public class CFB_View extends NCAAF_View {
+
+    public CFB_View(int year) {
+        super("cfb.ftl",year);
+    }
+
+
+}
