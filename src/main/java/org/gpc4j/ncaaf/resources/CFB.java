9fc142130fb5352fd3d38fcc68221a8ea1697956
lyle.harris@oracle.com <lyle.harris@oracle.com>
2017-12-27 13:53:39 -0800

    WIP

--- src/main/java/org/gpc4j/ncaaf/resources/CFB.java
+++ src/main/java/org/gpc4j/ncaaf/resources/CFB.java
@@ -16,12 +16,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import org.gpc4j.ncaaf.GamesProvider;
 import org.gpc4j.ncaaf.TeamProvider;
-import org.gpc4j.ncaaf.hystrix.GetTeamCommand;
 import org.gpc4j.ncaaf.hystrix.GetWeekCommand;
 import org.gpc4j.ncaaf.jaxb.Game;
 import org.gpc4j.ncaaf.jaxb.Team;
 import org.gpc4j.ncaaf.jaxb.Week;
 import org.gpc4j.ncaaf.views.AP_View;
+import org.gpc4j.ncaaf.views.CFB_View;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -32,22 +32,13 @@ import redis.clients.jedis.JedisPool;
  *
  * @author Lyle T Harris
  */
-@Path("{parameter: ap|AP}")
-public class AP {
+@Path("{parameter: cfb|CFB|cfp|CFP}")
+public class CFB extends NCAAF {
 
     final static private org.slf4j.Logger LOG
-            = LoggerFactory.getLogger(AP.class);
+            = LoggerFactory.getLogger(CFB.class);
 
     private Jedis jedis;
-
-    @Inject
-    private JedisPool pool;
-
-    @Inject
-    private GamesProvider gp;
-
-    @Inject
-    private TeamProvider tp;
 
 
     @PostConstruct
@@ -67,22 +58,24 @@ public class AP {
     @GET
     @Timed
     @Path("{year}")
-    public AP_View getYear(@PathParam("year") Integer year) throws Exception {
-        LOG.info(year.toString());
-        //LOG.debug("This: " + this);
-        //LOG.debug("Pool: " + pool);
+    public CFB_View getYear(@PathParam("year") Integer year) throws
+            InterruptedException, ExecutionException {
 
-        AP_View view = new AP_View(year);
-        view.setTitle(year + " AP Rankings");
-        view.setWeeks(getWeeks(year).collect(Collectors.toList()));
+        LOG.info(year.toString());
+
+        CFB_View view = new CFB_View(year);
+        view.setTitle(year + " College Football Playoff Rankings");
+        view.setWeeks(getWeeks("cfb", year).collect(Collectors.toList()));
         view.setTp(tp);
         view.setGp(gp);
+        
         return view;
     }
 
 
-    Stream<Week> getWeeks(int year) throws InterruptedException,
-            ExecutionException {
+    Stream<Week> getWeeks(final String poll, int year) throws
+            InterruptedException, ExecutionException {
+
         LOG.debug(year + "");
         final LinkedList<Week> weeks = new LinkedList<>();
         final List<Future<Week>> futures = new LinkedList<>();
@@ -90,27 +83,29 @@ public class AP {
         final int numWeeks = 18;
 
         // Submit all requests
-        for (int i = 0; i < numWeeks; i++) {
-            String key = "AP." + year + "." + i;
+        for (int i = 9; i < numWeeks; i++) {
+            String key = poll.toUpperCase() + "." + year + "." + i;
             futures.add(new GetWeekCommand(key, pool, tp).queue());
         }
 
         int xPosition = 0;
 
         // Collect the data
-        for (int i = 0; i < numWeeks; i++) {
+        int weekStart = 9;
+        for (Future<Week> future : futures) {
             xPosition += 250;
-            Week w = futures.get(i).get();
+            Week w = future.get();
             if (w.getTeams().isEmpty()) {
                 break;
             }
-            w.setNumber(i);
+            w.setNumber(weekStart++);
             w.setXPos(xPosition);
             final int pos = xPosition;
             w.getTeams().parallelStream().forEach((team) -> {
                 team.setCX(pos);
             });
             weeks.add(w);
+
         }
 
         // Get next weeks opponent
@@ -141,53 +136,6 @@ public class AP {
         }
 
         return weeks.parallelStream();
-    }
-
-
-    /**
-     * Use getNextGame instead.
-     *
-     * @deprecated
-     */
-    @Deprecated
-    Optional<Team> getNext(List<Game> games, String teamName) {
-
-        Optional<Team> next = Optional.empty();
-
-        for (Game game : games) {
-
-            String home = game.getHome();
-            String visitor = game.getVisitor();
-
-            if (teamName.equals(home)) {
-                LOG.trace(visitor + "@" + teamName);
-                next = Optional.of(tp.getTeam(visitor));
-            } else if (teamName.equals(visitor)) {
-                LOG.trace(teamName + "@" + home);
-                next = Optional.of(tp.getTeam(home));
-            }
-        }
-
-        return next;
-    }
-
-
-    Optional<Game> getNextGame(List<Game> games, String teamName) {
-
-        Optional<Game> next = Optional.empty();
-
-        for (Game game : games) {
-            if (game.getHomeScore() == null) {
-                // Game not played yet
-                if (teamName.equals(game.getHome())
-                        || teamName.equals(game.getVisitor())) {
-                    next = Optional.of(game);
-                    break;
-                }
-            }
-        }
-
-        return next;
     }
 
 
