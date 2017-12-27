0f2ede5536d285cbfb291ed5548b803c359afb3c
lyle.harris@oracle.com <lyle.harris@oracle.com>
2017-12-27 13:59:31 -0800

    Testing for Bowl games

--- src/test/java/org/gpc4j/ncaaf/BowlGamesTest.java
+++ src/test/java/org/gpc4j/ncaaf/BowlGamesTest.java
@@ -19,7 +19,7 @@ import redis.clients.jedis.JedisPoolConfig;
  *
  * @author Lyle T Harris
  */
-public class GamesProviderTest {
+public class BowlGamesTest {
 
     private static JedisPool pool;
 
@@ -28,7 +28,7 @@ public class GamesProviderTest {
     private static TeamProvider tp;
 
     final static private org.slf4j.Logger LOG
-            = LoggerFactory.getLogger(GamesProviderTest.class);
+            = LoggerFactory.getLogger(BowlGamesTest.class);
 
 
     @BeforeClass
@@ -43,116 +43,19 @@ public class GamesProviderTest {
     }
 
 
-    // @Test
-    public void stanfordAtRice() {
-        Team team = new Team();
-        team.setName("Stanford");
-        Game game = gp.getGame(team, 2017, 0).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Rice"));
-        assertThat(game.getVisitor(), is("Stanford"));
-    }
 
 
     @Test
-    public void stanfordAtUSC() {
+    public void orangeBowl2017() {
         Team team = new Team();
-        team.setName("Stanford");
-        Game game = gp.getGame(team, 2017, 1).get();
+        team.setName("Wisconsin");
+        Game game = gp.getGame(team, 2017, 14).get();
         LOG.info(game.toString());
-        assertThat(game.getHome(), is("USC"));
-        assertThat(game.getVisitor(), is("Stanford"));
+        assertThat(game.getHome(), is("Miami"));
+        assertThat(game.getVisitor(), is("Wisconsin"));
     }
 
 
-    //  @Test
-    public void michiganAtFlorida() {
-        Team team = new Team();
-        team.setName("Michigan");
-        Game game = gp.getGame(team, 2017, 0).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Florida"));
-        assertThat(game.getVisitor(), is("Michigan"));
-
-        team.setName("Florida");
-        game = gp.getGame(team, 2017, 0).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Florida"));
-        assertThat(game.getVisitor(), is("Michigan"));
-    }
-
-
-    @Test
-    public void cincinnatiAtMichigan() {
-        Team team = new Team();
-        team.setName("Michigan");
-        Game game = gp.getGame(team, 2017, 1).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Michigan"));
-        assertThat(game.getVisitor(), is("Cincinnati"));
-
-        team.setName("Cincinnati");
-        game = gp.getGame(team, 2017, 1).get();
-        LOG.info(game.toString());
-        assertThat(game.getVisitor(), is("Cincinnati"));
-        assertThat(game.getHome(), is("Michigan"));
-    }
-
-
-    @Test
-    public void AirForceAtMichigan() {
-        Team team = new Team();
-        team.setName("Michigan");
-        Game game = gp.getGame(team, 2017, 2).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Michigan"));
-        assertThat(game.getVisitor(), is("Air Force"));
-
-        team.setName("Air Force");
-        game = gp.getGame(team, 2017, 2).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Michigan"));
-        assertThat(game.getVisitor(), is("Air Force"));
-    }
-
-
-    @Test
-    public void MichiganBowlGame2016() {
-        Team team = new Team();
-        team.setName("Michigan");
-        Game game = gp.getGame(team, 2016, 14).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Florida State"));
-        assertThat(game.getVisitor(), is("Michigan"));
-
-        team.setName("Florida State");
-        game = gp.getGame(team, 2016, 14).get();
-        LOG.info(game.toString());
-        assertThat(game.getHome(), is("Florida State"));
-        assertThat(game.getVisitor(), is("Michigan"));
-
-    }
-
-
-    @Test
-    public void AirForceByeWeek() {
-        Team team = new Team();
-        team.setName("Air Force");
-        Optional<Game> opt = gp.getGame(team, 2017, 1);
-        Assert.assertFalse("Should be Bye Week", opt.isPresent());
-    }
-
-
-    // @Test
-    public void byTeamAndYear_Cincinnati2017() {
-
-        List<Game> games = gp.byTeamAndYear("Rice", 2017)
-                .collect(Collectors.toList());
-
-        for (Game game : games) {
-            LOG.info(game.toString());
-        }
-    }
 
 
 }
