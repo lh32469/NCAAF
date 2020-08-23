a9ccf7d03b1e27fe1009082d9aa4795bc9e783ee
Lyle Harris <receipts@gpc4j.org>
2020-08-23 13:42:18 -0700

    Register new container with ID for Service

--- updateConsul.sh
+++ updateConsul.sh
@@ -7,7 +7,9 @@ IP=${IP%%[[:space:]]}
 # Consul server is Docker host
 HOST=$(ip route | grep default | awk '{ print $3 }')
 
-curl -X PUT -d "{\"Name\": \"$(hostname)\", \
+curl -X PUT -d "{\
+	\"Name\": \"${SERVICE}\", \
+	\"ID\": \"$(hostname)\", \
 	\"Address\": \"${IP}\" }" \
  http://${HOST}:8500/v1/agent/service/register
 
