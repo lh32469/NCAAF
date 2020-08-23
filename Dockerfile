ec3a9eb830744602a074fe165558d48a7e030617
Lyle Harris <receipts@gpc4j.org>
2020-08-23 15:52:05 -0700

    Deregister with Consul via signal trap

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,6 @@ COPY updateConsul.sh    /usr/src
 COPY runApp.sh          /usr/src
 WORKDIR                 /usr/src/
 
-COPY S01consulRegistration /etc/rc3.d
-COPY S01consulRegistration /etc/rcS.d
-
-COPY S01consulRegistration /etc/rc0.d/K01consulRegistration
-COPY S01consulRegistration /etc/rc6.d/K01consulRegistration
-
 EXPOSE 9020
 
 CMD ./runApp.sh
--- runApp.sh
+++ runApp.sh
@@ -1,2 +1,23 @@
+
+# Consul server is Docker host
+HOST=$(ip route | grep default | awk '{ print $3 }')
+
+deregister() {
+    echo "Container stopped, performing cleanup..."
+    curl -X PUT http://${HOST}:8500/v1/agent/service/deregister/$(hostname)
+}
+
+trap 'deregister' SIGTERM
+
+IP=$(hostname -I)
+# Strip trailing \n
+IP=${IP%%[[:space:]]}
+
+curl -X PUT -d "{\
+	\"Name\": \"${SERVICE}\", \
+	\"ID\": \"$(hostname)\", \
+	\"Address\": \"${IP}\" }" \
+  http://${HOST}:8500/v1/agent/service/register
+
 java -jar ncaaf.jar server docker.yml
 
