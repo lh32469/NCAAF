2c6540a622d4191d939a5c9c593c7871a6d6dfc5
Lyle Harris <receipts@gpc4j.org>
2020-08-23 16:58:43 -0700

    trap testing

--- runApp.sh
+++ runApp.sh
@@ -1,13 +1,14 @@
 
-# Consul server is Docker host
-HOST=$(ip route | grep default | awk '{ print $3 }')
+trap deregister 1 2 3 6 15
 
 deregister() {
     echo "Container stopped, performing cleanup..."
     curl -X PUT http://${HOST}:8500/v1/agent/service/deregister/$(hostname)
+    exit
 }
 
-trap 'deregister' SIGTERM
+# Consul server is Docker host
+HOST=$(ip route | grep default | awk '{ print $3 }')
 
 IP=$(hostname -I)
 # Strip trailing \n
